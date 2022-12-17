a07e4d765559105df7f9297413c9ced3ef075320
Yukai Huang <yukaihuangtw@gmail.com>
2022-12-17 15:13:16 +0800

    fix: logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,16 +2,14 @@ import winston from 'winston';
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple(),
+  ),
+});
+
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
 });
 
 //
@@ -22,6 +20,18 @@ if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
+    }),
+  );
+
+  logger.add(
+    new winston.transports.Console({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+        winston.format.timestamp(),
+        customFormat,
+      ),
     }),
   );
 }
