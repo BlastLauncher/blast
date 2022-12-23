ca2ba5f036297ef1f1ef70727eb1a10e9c0fdebd
Yukai Huang <yukaihuangtw@gmail.com>
2022-12-24 00:39:10 +0800

    style: setup react hooks eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -11,7 +17,9 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
   settings: {
     react: {
       version: "detect",
