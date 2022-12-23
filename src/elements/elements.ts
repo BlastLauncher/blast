d843081a020538bac6c6241f9c09793bd9eea1fc
Yukai Huang <yukaihuangtw@gmail.com>
2022-12-23 20:57:26 +0800

    refactor: rename primitives elements

--- src/elements/elements.ts
+++ src/elements/elements.ts
@@ -1,13 +1,8 @@
 import { IElement } from "./BaseElement";
-import { Action, ActionPanel, EmptyView, List, View, Detail } from "./ExtendedElements";
+import * as Primitives from "./PrimitiveElements";
 import Command from "./Command";
 
 export const elements: { [key: string]: IElement } = {
-  View,
-  ActionPanel,
-  List,
   Command,
-  EmptyView,
-  Action,
-  Detail,
+  ...Primitives,
 };
