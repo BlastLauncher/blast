7cea497c8b9e9878d847d6278ebd7e09351c1c9e
Yukai Huang <yukaihuangtw@gmail.com>
2023-04-23 22:55:59 +0800

    refactor: window things

--- apps/electron-client/src/tray.ts
+++ apps/electron-client/src/tray.ts
@@ -1,13 +1,15 @@
 import { app, Menu, Tray } from "electron";
 import path from "path";
 
+import { showMainWindow } from "./window";
+
 export const createTray = (): void => {
   const tray = new Tray(path.join(__dirname, "../main/assets/Icon-Template.png"));
   const contextMenu = Menu.buildFromTemplate([
     {
       label: "Show App",
       click: function() {
-        app.show();
+        showMainWindow();
       },
     },
     {
