afb561f9f61258ae66f4368f89d32ded29ab5995
Yukai Huang <yukaihuangtw@gmail.com>
2023-06-11 21:13:35 +0800

    fix: esbuild built runtime

--- packages/blast-runtime/build.js
+++ packages/blast-runtime/build.js
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require("esbuild");
-// const { nodeExternalsPlugin } = require("esbuild-node-externals");
+const { nodeExternalsPlugin } = require("esbuild-node-externals");
 
 esbuild.build({
   entryPoints: ["./src/run.ts"],
   bundle: true,
   platform: "node",
   outfile: "dist/run.cjs",
+  keepNames: true,
   plugins: [
+    nodeExternalsPlugin({
+      // packagePath: "./package.json",
+      // allowList: ["@blastlauncher/api", "react"],
+      // allowList: ["react"]
+      dependencies: false,
+      // allowList: ["react", "@raycast/api", "react-reconciler", "@blastlauncher/api", "@blastlauncher/renderer"]
+    }),
   ],
 });
--- packages/blast-runtime/src/components/CommandList/utils.ts
+++ packages/blast-runtime/src/components/CommandList/utils.ts
@@ -1,21 +1,51 @@
 import fs from "fs";
+import os from "os";
+import path from "path";
 
+import * as blastAPI from "@blastlauncher/api";
 import _eval from "eval";
 import React from "react";
 
+// hook blastAPI to require('@raycast/api')
+
+// const extensionRootPackagePath = path.join(os.homedir(), '.blast', 'extensions')
+// process.chdir(extensionRootPackagePath)
+
 export function evalCommandModule(pkg: string) {
+  // console.log("evalCommandModule", pkg);
+  // try {
+  //   // eslint-disable-next-line @typescript-eslint/no-var-requires
+  //   return globalThis["" + "require"]?.(pkg).default;
+  // } catch (e) {
+  //   console.error(e);
+  //   return null;
+  // }
+
   const jsCode = fs.readFileSync(pkg, "utf8");
+
+  // TODO: might need to hook into the require function to make sure
+  const customRequire = (id: string) => {
+    if (id === "react") {
+      return React;
+    } else if (id === "@raycast/api") {
+      return blastAPI;
+    } else {
+      return require(id);
+    }
+  };
 
   const mod: any = _eval(
     jsCode,
     pkg,
     {
-      require,
+      require: customRequire,
       _jsx: React.createElement,
       _jsxFragment: React.Fragment,
     },
     true
   );
+
+  // TODO: do additional checks to make sure the module is valid
 
   // If we use rollup for bundling, we can use the following:
   // return mod;
