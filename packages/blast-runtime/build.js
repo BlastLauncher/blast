884160503b10adf660c711454ddcef18f1387d76
Yukai Huang <yukaihuangtw@gmail.com>
2023-06-11 21:19:54 +0800

    fix: bundle completly

--- packages/blast-runtime/build.js
+++ packages/blast-runtime/build.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require("esbuild");
-const { nodeExternalsPlugin } = require("esbuild-node-externals");
 
 esbuild.build({
   entryPoints: ["./src/run.ts"],
@@ -8,13 +7,4 @@ esbuild.build({
   platform: "node",
   outfile: "dist/run.cjs",
   keepNames: true,
-  plugins: [
-    nodeExternalsPlugin({
-      // packagePath: "./package.json",
-      // allowList: ["@blastlauncher/api", "react"],
-      // allowList: ["react"]
-      dependencies: false,
-      // allowList: ["react", "@raycast/api", "react-reconciler", "@blastlauncher/api", "@blastlauncher/renderer"]
-    }),
-  ],
 });
