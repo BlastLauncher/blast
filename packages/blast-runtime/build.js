b63cf5f3bd850528184bf59cbf5427e13ac66a84
Yukai Huang <yukaihuangtw@gmail.com>
2023-08-18 21:50:35 +0800

    feat: add esbuile watch for runtime

--- packages/blast-runtime/build.js
+++ packages/blast-runtime/build.js
@@ -1,10 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require("esbuild");
 
-esbuild.build({
+const watch = process.argv.includes("--watch");
+
+const esbuildConfig = {
   entryPoints: ["./src/run.ts"],
   bundle: true,
   platform: "node",
   outfile: "dist/run.cjs",
   keepNames: true,
-});
+};
+
+if (watch) {
+  esbuild.context(esbuildConfig).then((ctx) => {
+    ctx.watch()
+  });
+} else {
+  esbuild.build(esbuildConfig).catch(() => process.exit(1));
+}
