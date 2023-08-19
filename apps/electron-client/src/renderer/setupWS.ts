af6d0d78df50c3ad23db0be8a6bad4e2a1c27d6a
Yukai Huang <yukaihuangtw@gmail.com>
2023-08-19 10:07:43 +0800

    feat: ws reconnect

--- apps/electron-client/src/renderer/setupWS.ts
+++ apps/electron-client/src/renderer/setupWS.ts
@@ -1,7 +1,9 @@
 import { Client } from "rpc-websockets";
 
 export function setupWS(onOpened: (ws: Client) => void) {
-  const ws = new Client("ws://localhost:8763");
+  const ws = new Client("ws://localhost:8763", {
+    reconnect: true,
+  });
 
   ws.on("open", function () {
     onOpened(ws);
