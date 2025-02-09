fb41363864619abc6be7c264c2a189bdefbc98f7
Yukai Huang <yukaihuangtw@gmail.com>
2025-02-09 23:09:46 +0800

    feat(environment): add support for async context in environment handling

--- packages/blast-api/src/environment/index.ts
+++ packages/blast-api/src/environment/index.ts
@@ -1,21 +1,36 @@
-import { type Environment, LaunchType } from '../LaunchType'
+import { AsyncLocalStorage } from "node:async_hooks";
 
-export let environment: Environment = {
-  assetsPath: '',
-  commandMode: 'view',
-  commandName: '',
-  extensionName: '',
+import { type Environment, LaunchType } from "../LaunchType";
+
+const asyncLocalStorage = new AsyncLocalStorage<Environment>();
+
+const defaultEnvironment: Environment = {
+  assetsPath: "",
+  commandMode: "view",
+  commandName: "",
+  extensionName: "",
   isDevelopment: false,
   launchType: LaunchType.UserInitiated,
-  raycastVersion: '',
-  supportPath: '',
-  textSize: 'medium',
-  theme: 'light',
-}
+  raycastVersion: "",
+  supportPath: "",
+  textSize: "medium",
+  theme: "light",
+};
 
-export function prepareEnvironment(env: Partial<Environment>) {
-  environment = {
-    ...environment,
-    ...env,
-  }
+export const environment: Environment = new Proxy(defaultEnvironment, {
+  get(target, prop, receiver) {
+    const store = asyncLocalStorage.getStore();
+    const data = store || target;
+    return Reflect.get(data, prop, receiver);
+  },
+  set(target, prop, value, receiver) {
+    const store = asyncLocalStorage.getStore();
+    const data = store || target;
+    return Reflect.set(data, prop, value, receiver);
+  },
+});
+
+export function prepareEnvironment<T>(env: Partial<Environment>, callback: () => T): T {
+  const newEnv = { ...defaultEnvironment, ...env };
+  return asyncLocalStorage.run(newEnv, callback);
 }
